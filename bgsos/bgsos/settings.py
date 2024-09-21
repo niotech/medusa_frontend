9f5d9f2b14b8a43b9e32d5aa35ee314f6fffdbf3
Benny Christanto <niotech5@gmail.com>
2024-09-21 12:54:07 +0800

    chore: tinymc

--- bgsos/bgsos/settings.py
+++ bgsos/bgsos/settings.py
@@ -68,6 +68,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
     
+    'tinymce',
+
     'web',
 ]
 
@@ -201,4 +203,21 @@ LOGGING = {
             "propagate": False,
         },
     },
+}
+
+
+TINYMCE_JS_URL = STATIC_URL + "tinymce/tinymce.min.js"
+TINYMCE_DEFAULT_CONFIG = {
+    "height": "320px",
+    "width": "960px",
+    "menubar": "file edit view insert format tools table help",
+    "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
+    "fullscreen insertdatetime media table paste code help wordcount spellchecker",
+    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
+    "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
+    "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
+    "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
+    "a11ycheck ltr rtl | showcomments addcomment code",
+    "custom_undo_redo_levels": 10,
+    "language": "en_US",  # To force a specific language instead of the Django current language.
 }
